4497894985db135506c27cd96693c8f524132132
chonlapat323 <cschonlapat@gmail.com>
2025-05-19 00:23:01 +0700

    feat(logout): change logout service to logoutRequest

--- components/HomePageClient.tsx
+++ components/HomePageClient.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { logout } from "@/services/auth.service";
+import { logoutRequest } from "@/services/auth.service";
 
 export default function HomePageClient() {
   return (
     <>
       <button
-        onClick={logout}
+        onClick={logoutRequest}
         className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
       >
         Logout
